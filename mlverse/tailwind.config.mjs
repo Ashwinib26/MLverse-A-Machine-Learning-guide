a0fe87756eaa33a4ff1118fb7d958b583d259d93
Ashwini Bisen <ashwinibisen26@gmail.com>
2025-06-19 16:59:48 +0530

    Feature Update-Home Page

--- mlverse/tailwind.config.mjs
+++ mlverse/tailwind.config.mjs
@@ -1,15 +1,12 @@
-// tailwind.config.mjs
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-const config = {
-  darkMode: 'class',
+module.exports = {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-export default config;
